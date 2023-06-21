508f1e3355b4f970d086c615dd362f9955bf6099
russelelele <adam.le.chance@gmail.com>
2023-06-21 22:49:42 +0300

    Create CREATE TABLE Users (.sql

---
+++ CREATE TABLE Users (.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users ( 
+    uers_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    name VARCHAR(50),
+    surname VARCHAR(30),
+    age DECIMAL(8, 0), 
+    address VARCHAR(100) );
