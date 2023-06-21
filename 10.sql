b7fd335eb8c14fa3168d6ed44cea98a0a2a1c714
russelelele <88386412+russelelele@users.noreply.github.com>
2023-06-21 23:33:13 +0300

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,4 +1,4 @@
 Select FirstName, LastName, City, SUM(Amount)
 From  Orders Inner  Join Customers
   on  Orders.CustomerID = Customers.CustomerID
-  Groub by FirstName, LastName
+  Group by FirstName, LastName
